d3f9f61ab2a59fa1fc49eac2c03daf00bd4ade67
ivansukach <vsukach1999@gmail.com>
2020-04-06 22:35:35 +0300

    #4

---
+++ static-content/precache-manifest.1129149bb3814003f042c0bda1a3e169.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79558e34635549cab82cf926442918b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f809feaf07ec3eeb15aa",
+    "url": "/static/css/main.b7e36adc.chunk.css"
+  },
+  {
+    "revision": "2ba37ea0f52b96ff907d",
+    "url": "/static/js/2.c322041b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c322041b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f809feaf07ec3eeb15aa",
+    "url": "/static/js/main.00821373.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  },
+  {
+    "revision": "886507f9c7a1c2ed2d8bbb7e87d0689a",
+    "url": "/static/media/BancoDi.886507f9.ttf"
+  },
+  {
+    "revision": "751d7b6c4391708b7e54c7154ec5581b",
+    "url": "/static/media/bg.751d7b6c.jpg"
+  },
+  {
+    "revision": "6a167556472e8793acaaf04a0c846133",
+    "url": "/static/media/d_Charger.6a167556.otf"
+  },
+  {
+    "revision": "de239fc7db1c71cf7b4505d52229b3b1",
+    "url": "/static/media/giphy.de239fc7.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36534e3c03ea6105fdd1118b864b2970",
+    "url": "/static/media/myface.36534e3c.jpg"
+  },
+  {
+    "revision": "50e7012b5be57e77bea18ad1e779e762",
+    "url": "/static/media/pikachu.50e7012b.gif"
+  },
+  {
+    "revision": "dca56646596fd241295bd8835c27d619",
+    "url": "/static/media/pirates.dca56646.mp3"
+  },
+  {
+    "revision": "edfa16b9ee2d02d04258859e7df7df2a",
+    "url": "/static/media/tortose.edfa16b9.gif"
+  }
+]);
